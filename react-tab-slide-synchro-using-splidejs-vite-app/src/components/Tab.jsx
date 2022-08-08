1bdfa814d86f4fa594c2a8e437371d44b6dafae9
Higashi-Kota <higashi.kota@gri.jp>
2022-08-08 16:56:49 +0900

    update sample

--- react-tab-slide-synchro-using-splidejs-vite-app/src/components/Tab.jsx
+++ react-tab-slide-synchro-using-splidejs-vite-app/src/components/Tab.jsx
@@ -1,4 +1,7 @@
+import {motion, useAnimationControls} from 'framer-motion';
 import {css, cx} from '@emotion/css';
+import {useEffect} from 'react';
+import {useState} from 'react';
 
 const Tab = ({
   tabsDomRef,
@@ -7,18 +10,41 @@ const Tab = ({
   splideInstanceMoveRef,
   splideInstanceControllerRef,
 }) => {
+  const bgControls = useAnimationControls();
+  const [left, setLeft] = useState(0);
+
+  useEffect(() => {
+    bgControls.start({
+      opacity: 1,
+      x: left,
+    });
+  }, [left]);
+
+  useEffect(() => {
+    const dom = tabsDomRef[activeSlideIndex].current;
+    setLeft(dom.offsetLeft);
+  }, [activeSlideIndex]);
+
   return (
     <ul
       className={css`
+        position: relative;
         list-style: none;
         display: flex;
         justify-content: center;
         align-items: center;
         li {
-          padding: 0.5rem;
+          transition: color 250ms ease;
+          padding: 5px;
           cursor: pointer;
+          min-width: 5.5rem;
+          min-height: 2rem;
+          display: flex;
+          justify-content: center;
+          align-items: center;
           &.active {
-            background: #f6f6f6;
+            color: rgb(255, 255, 255);
+            /* background: #f6f6f6; */
           }
         }
       `}
@@ -48,6 +74,24 @@ const Tab = ({
           </li>
         );
       })}
+      <motion.li
+        animate={bgControls}
+        className={css`
+          position: absolute;
+          top: 0;
+          left: 0;
+          opacity: 0;
+          z-index: -1;
+          /* background: #f6f6f6; */
+          background-color: rgb(79, 101, 241);
+          background-image: linear-gradient(
+            90deg,
+            rgb(111, 137, 251) 0%,
+            rgb(97, 109, 245) 33%,
+            rgb(92, 82, 235) 100%
+          );
+        `}
+      />
     </ul>
   );
 };
